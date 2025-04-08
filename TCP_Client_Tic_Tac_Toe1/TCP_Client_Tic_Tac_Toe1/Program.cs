5f7331c83634c8cf1bf4c0cfc994019f76271b8f
Neoray Banun <zoonbbb16@gmail.com>
2025-04-08 16:23:15 +0300

    Code Files

---
+++ TCP_Client_Tic_Tac_Toe1/TCP_Client_Tic_Tac_Toe1/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_Client_Tic_Tac_Toe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+            Console.WriteLine("Connected to the server");
+
+            byte[] buffer = new byte[1024];
+            int byteRead = stream.Read(buffer, 0, buffer.Length);
+            string symbol = UTF8Encoding.UTF8.GetString(buffer, 0, byteRead);
+
+            Console.WriteLine($"You are ({symbol})");
+
+            Recive(stream);
+
+        }
+
+        public static void Recive(NetworkStream stream)
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int byteRead = stream.Read(buffer, 0, buffer.Length);
+                string message = UTF8Encoding.UTF8.GetString(buffer, 0, byteRead);
+
+                if (message.StartsWith("BOARD:"))
+                {
+                    message = message.Substring(6);
+                    char[,] board = StringToBoard(message);
+                    DisplayBoardAndSend(board, stream);
+                }
+                else if (message.StartsWith("MSG:"))
+                {
+                    message = message.Substring(4);
+                    Console.WriteLine("");
+                    Console.WriteLine(message);
+                }
+                else if (message.StartsWith("RES:"))
+                {
+                    message = message.Substring(4);
+                    Console.WriteLine(message);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+
+        public static char[,] StringToBoard(string stringBoard)
+        {
+            int index = 0;
+            char[,] board = { {' ', ' ', ' '}
+                             ,{' ', ' ', ' '}
+                             ,{' ', ' ', ' '} };
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = stringBoard[index];
+                    index++;
+                }
+            }
+
+            return board;
+        }
+
+        public static void DisplayBoardAndSend(char[,] board, NetworkStream stream)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"    [1]   [2]   [3]");
+            Console.WriteLine("");
+            Console.WriteLine($"[1]  {board[0, 0]}  │  {board[0, 1]}  │  {board[0, 2]}");
+            Console.WriteLine("    ─── + ─── + ───");
+            Console.WriteLine($"[2]  {board[1, 0]}  │  {board[1, 1]}  │  {board[1, 2]}");
+            Console.WriteLine("    ─── + ─── + ───");
+            Console.WriteLine($"[3]  {board[2, 0]}  │  {board[2, 1]}  │  {board[2, 2]}");
+
+            Console.WriteLine("\nWrite the position you would like to place");
+
+            int pos;
+
+            while (true)
+            {
+                Console.WriteLine("Write only two numbers like (X position) (Y position) starting at 1");
+                string message = Console.ReadLine();
+                string result = message.Trim();
+
+                if (int.TryParse(result, out pos))
+                {
+                    int posX = pos / 10;
+                    int posY = pos % 10;
+
+                    if (posX < 1 || posX > 3 || posY < 1 || posY > 3 || pos < 10 || pos > 99)
+                    {
+                        Console.WriteLine("\nNot in the compounds of the board!");
+                    }
+                    else
+                    {
+                        if (board[posY - 1, posX - 1] != ' ')
+                        {
+                            Console.WriteLine("\nThe position is already taken!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nWaiting for opponent response...");
+                            break;
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nNot a valid position!");
+                }
+            }
+
+            string posStr = pos.ToString();
+            byte[] buffer = UTF8Encoding.UTF8.GetBytes(posStr);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ TCP_Server_Tic_Tac_Toe/TCP_Server_Tic_Tac_Toe/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP_Server_Tic_Tac_Toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Win = false;
+
+            char[,] board = { {' ', ' ', ' '}
+                             ,{' ', ' ', ' '}
+                             ,{' ', ' ', ' '} };
+
+
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server started");
+
+            TcpClient playerX = server.AcceptTcpClient();
+            NetworkStream xStream = playerX.GetStream();
+
+            Console.WriteLine("Player 1 connected");
+            Send("X", xStream);
+            Send("Waiting for the other player to join...", xStream);
+
+            TcpClient playerO = server.AcceptTcpClient();
+            NetworkStream oStream = playerO.GetStream();
+
+            Console.WriteLine("Player 2 connected");
+            Send("O", oStream);
+
+            server.Stop();
+
+            for (int i = 0; i < 9; i++)
+            {
+                DisplayBoard(board);
+                if (i % 2 == 0)
+                {
+                    SendBoard(board, xStream);
+
+                    string strPos = Recive(xStream);
+                    int pos = int.Parse(strPos);
+                    int posX = (pos / 10) - 1;
+                    int posY = (pos % 10) - 1;
+
+                    board[posY, posX] = 'X';
+                    Win = CheckWin(board, 'X');
+
+                    if (Win)
+                    {
+                        Console.WriteLine("X won");
+
+                        Send("RES:You won!", xStream);
+                        Send("RES:You lost :(", oStream);
+                        break;
+                    }
+                }
+                else if (i % 2 == 1)
+                {
+                    SendBoard(board, oStream);
+
+                    string strPos = Recive(oStream);
+                    int pos = int.Parse(strPos);
+                    int posX = (pos / 10) - 1;
+                    int posY = (pos % 10) - 1;
+
+                    board[posY, posX] = 'O';
+                    Win = CheckWin(board, 'O');
+
+                    if (Win)
+                    {
+                        Console.WriteLine("\nO won");
+
+                        Send("RES:You won!", oStream);
+                        Send("RES:You lost :(", xStream);
+                        break;
+                    }
+                }
+            }
+
+            if (!Win)
+            {
+                Console.WriteLine("It is a tie");
+                Send("RES:It is a tie", oStream);
+                Send("RES:It is a tie", xStream);
+
+            }
+        }
+
+        public static bool CheckWin(char[,] board, char player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
+                {
+                    return true;
+                }
+                else if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
+                {
+                    return true;
+                }
+            }
+
+            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+            {
+                return true;
+            }
+            else if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void DisplayBoard(char[,] board)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"    [1]   [2]   [3]");
+            Console.WriteLine("");
+            Console.WriteLine($"[1]  {board[0, 0]}  │  {board[0, 1]}  │  {board[0, 2]}");
+            Console.WriteLine("    ─── + ─── + ───");
+            Console.WriteLine($"[2]  {board[1, 0]}  │  {board[1, 1]}  │  {board[1, 2]}");
+            Console.WriteLine("    ─── + ─── + ───");
+            Console.WriteLine($"[3]  {board[2, 0]}  │  {board[2, 1]}  │  {board[2, 2]}");
+        }
+
+        public static void Send(string message, NetworkStream stream)
+        {
+            if (message == "X" || message == "O")
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                string formattedMessage = "MSG:" + message;
+                byte[] buffer = Encoding.UTF8.GetBytes(formattedMessage);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+
+        }
+
+
+        public static string Recive(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            int byteRead = stream.Read(buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer, 0, byteRead);
+        }
+
+        public static void SendBoard(char[,] board, NetworkStream stream)
+        {
+            string stringBoard = string.Concat(board.Cast<char>());
+            string formattedBoard = "BOARD:" + stringBoard;
+            byte[] buffer = Encoding.UTF8.GetBytes(formattedBoard);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
